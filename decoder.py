b0674cb483c717b723b8a5d99656730a8a53dc0c
SeT[X] <setx@s3tx.ru>
2013-09-13 21:45:54 +0200

    decoder v0.4b more decoding ethods

--- decoder.py
+++ decoder.py
@@ -22,21 +22,21 @@
 """
     Tipical and most used combinations
     
-    eval(gzinflate(base64_decode('Code')))
-    eval(gzinflate(str_rot13(base64_decode('Code'))))
-    eval(gzinflate(base64_decode(str_rot13('Code'))))
-    eval(gzinflate(base64_decode(base64_decode(str_rot13('Code')))))
-    eval(gzuncompress(base64_decode('Code')))
-    eval(gzuncompress(str_rot13(base64_decode('Code'))))
-    eval(gzuncompress(base64_decode(str_rot13('Code'))))
-    eval(base64_decode('Code'))
-    eval(str_rot13(gzinflate(base64_decode('Code'))))
-    eval(gzinflate(base64_decode(strrev(str_rot13('Code')))))
-    eval(gzinflate(base64_decode(strrev('Code'))))
-    eval(gzinflate(base64_decode(str_rot13('Code'))))
-    eval(gzinflate(base64_decode(str_rot13(strrev('Code')))))
-    eval(base64_decode(gzuncompress(base64_decode('Code'))))
-    eval(gzinflate(base64_decode(rawurldecode('Code'))))
+    -s "i>b"     -> eval(gzinflate(base64_decode('Code')))
+    -s "i>r>b"   -> eval(gzinflate(str_rot13(base64_decode('Code'))))
+    -s "i>b>r"   -> eval(gzinflate(base64_decode(str_rot13('Code'))))
+    -s "i>b>b>r" -> eval(gzinflate(base64_decode(base64_decode(str_rot13('Code')))))
+    -s "u>b"     -> eval(gzuncompress(base64_decode('Code')))
+    -s "u>r>b"   -> eval(gzuncompress(str_rot13(base64_decode('Code'))))
+    -s "u>b>r"   -> eval(gzuncompress(base64_decode(str_rot13('Code'))))
+    -b           -> eval(base64_decode('Code'))
+    -s "r>i>b"   -> eval(str_rot13(gzinflate(base64_decode('Code'))))
+    -s "i>b>t>r" -> eval(gzinflate(base64_decode(strrev(str_rot13('Code')))))
+    -s "i>b>t"   -> eval(gzinflate(base64_decode(strrev('Code'))))
+    -s "i>b>r"   -> eval(gzinflate(base64_decode(str_rot13('Code'))))
+    -s "i>b>r>t" -> eval(gzinflate(base64_decode(str_rot13(strrev('Code')))))
+    -s "b>u>b"   -> eval(base64_decode(gzuncompress(base64_decode('Code'))))
+    -s "i>b>l"   -> eval(gzinflate(base64_decode(rawurldecode('Code'))))
 """
 
 
@@ -44,7 +44,7 @@
 from sys import argv, exit
 import zlib
 import argparse
-
+from urllib import quote, unquote
 
 
 #----------------------------------------------------------------------
@@ -168,10 +168,54 @@ class decoder:
                 return '[!] Error HEX encoding: %s.' % e
             
     #----------------------------------------------------------------------
+    def rot_13(self, data = None, encode = False):
+        """"""
+        try:
+            if not encode:
+                if data:
+                    return data.decode("rot_13")
+                else:
+                    return self.data.decode("rot_13")
+            else:
+                if data:
+                    return data.encode("rot_13")
+                else:
+                    return self.data.encode("rot_13")
+        except Exception, e:
+            return '[!] Error Rot_13: %s.' % e
+        
+    #----------------------------------------------------------------------
+    def strrev(self, data = None):
+        """"""
+        try:
+            if data:
+                return data[::-1]
+            else:
+                return self.data[::-1]
+        except Exception, e:
+            return '[!] Error strrev: %s.' % e
+        
+    #----------------------------------------------------------------------
+    def rawurldecode(self, data = None, encode = False):
+        """"""
+        try:
+            if not encode:
+                if data:
+                    return unquote(data)
+                else:
+                    return unquote(self.data)
+            else:
+                if data:
+                    return quote(data)
+                else:
+                    return quote(self.data)
+        except Exception, e:
+            return '[!] Error rawurldecode: %s.' % e
+    #----------------------------------------------------------------------
     def output_Write(self, output_file, data):
         """Writes the parsed output to file"""
         try:
-            f = open(output_file, 'w')
+            f = open(output_file, 'wb')
             f.write(data)
             f.close
             return '[*] Work Done! File "%s" has been saved.' % output_file
@@ -209,14 +253,21 @@ class decoder:
             return self.zlib_gzun(compress = encode, data = data)
         elif method == 'x':
             return self.hex(encode = encode, data = data)
+        elif method == 'r':
+            return self.rot_13(encode = encode, data = data)
+        elif method == 't':
+            return self.strrev(data = data)
+        elif method == 'l':
+            return self.rot_13(encode = encode, data = data)
         else:
             return '[!] Error Sequence: No data recieved as method.'
 
 
 #----------------------------------------------------------------------
-def main(input_data, file, encode = False, xor = False, base64 = False, zlib_gzun = False, hex = False, output_file = None, zlib_gzin = False, sequence = False):
+def main(input_data, file, encode = False, xor = False, base64 = False, zlib_gzun = False, hex = False, output_file = None, zlib_gzin = False, sequence = False, rot_13 = False, strrev = False, rawurldecode = False):
     """Simple main function"""
-
+    
+    
     out = ''
     
     if input_data:
@@ -252,7 +303,18 @@ def main(input_data, file, encode = False, xor = False, base64 = False, zlib_gzu
             out = dec.hex(encode = True)
         else:
             out = dec.hex()
-        
+    elif rot_13:
+        if encode:
+            out = dec.rot_13(encode = True)
+        else:
+            out = dec.rot_13()
+    elif strrev:
+        out = dec.strrev()
+    elif rawurldecode:
+        if encode:
+            out = dec.rawurldecode(encode = True)
+        else:
+            out = dec.rawurldecode()
     
     if output_file:
         print dec.output_Write(output_file = output_file, data = out)
@@ -267,7 +329,7 @@ if __name__ == '__main__':
 
     
     
-    parser = argparse.ArgumentParser(description='Version - Decoder v0.3b', prog='Decoder.py', usage=' Main options \n- %(prog)s [-e] (-s/-b/-i/-u/-x) ( -i data / -f file ) [-o output_file]\n', epilog='Для получения любой информации обращайтесь всё тудаже')
+    parser = argparse.ArgumentParser(description='Version - Decoder v0.4b', prog='Decoder.py', usage=' Main options \n- %(prog)s [-e] (-s/-b/-i/-u/-x) ( -i data / -f file ) [-o output_file]\n', epilog='Для получения любой информации обращайтесь всё тудаже')
     
     gr1 = parser.add_argument_group('Main Options')
     gr1.add_argument('-f', '--file=', action='store', dest='file', default=None, help='Text file with data to be decoded or encoded')
@@ -278,13 +340,18 @@ if __name__ == '__main__':
     gr1.add_argument('-u', action='store_true', dest='zlib_gzun', default=False, help='Zlib "gzuncompress" compress/decompress method')
     gr1.add_argument('-i', action='store_true', dest='zlib_gzin', default=False, help='Zlib "gzinflate" compress/decompress method')
     gr1.add_argument('-x', action='store_true', dest='hex', default=False, help='Hex encode/decode method')
+    gr1.add_argument('-r', action='store_true', dest='rot_13', default=False, help='rot_13 encode/decode method')
+    gr1.add_argument('-t', action='store_true', dest='strrev', default=False, help='strrev encode/decode method')
+    gr1.add_argument('-l', action='store_true', dest='rawurldecode', default=False, help='rawurldecode encode/decode method')
     gr1.add_argument('-s', action='store', dest='sequence', default=None, help='Decoding sequence e.g.(-s "b>i>b")')
     gr1.add_argument('-v', '--version', action='version', version='%(prog)s v0.3b')
-
+    
+    
+    
     checkArgs()
     args = parser.parse_args()
-    
-    main(input_data = args.input_data, file = args.file, encode = args.encode, xor = False, base64 = args.base64, zlib_gzun = args.zlib_gzun, hex = args.hex, output_file = args.output_file, zlib_gzin = args.zlib_gzin, sequence = args.sequence)
+    xor = False
+    main(args.input_data, args.file, args.encode, xor, args.base64, args.zlib_gzun, args.hex, args.output_file, args.zlib_gzin, args.sequence, args.rot_13, args.strrev, args.rawurldecode)
     
 
 
